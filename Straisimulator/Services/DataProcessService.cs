61791d16defd895d27ed000ba48fcc68926c5bfc
toringebrekka2 <59830058+toringebrekka2@users.noreply.github.com>
2022-04-04 10:08:01 +0200

    nyeste

--- Straisimulator/Services/DataProcessService.cs
+++ Straisimulator/Services/DataProcessService.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Straisimulator.Services;
 
 public class DataProcessService
@@ -7,10 +9,21 @@ public class DataProcessService
         
     }
     
-    /*public List<int> getOpAndCykTime(string inputText)
+    public List<int> getOpAndCykTime(string inputText)
     {
         string pattern = @"tid: (?<operasjonstid>\d{2}:\d{2}) .+tid: (?<cykeltid>\d{2}:\d{2})";
-    }*/
+        Match match = Regex.Match(inputText, pattern);
+
+        if (match.Success)
+        {
+            string test = match.Groups["operationstid"].Value;
+            string test2 = match.Groups["cykeltid"].Value;
+
+            TimeSpan time = new TimeSpan(0, 0, Convert.ToInt32(test.Substring(0, 2)),
+                Convert.ToInt32(test.Substring(3, 2)));
+        }
+        return new List<int>();
+    }
 
     public bool checkForOperation(string input)
     {
