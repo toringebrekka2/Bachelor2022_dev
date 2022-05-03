2c1374e035766e192eb1e34f567ec51a10de3b25
anderskorsnes <anders.korsnes@hotmail.com>
2022-05-03 22:41:56 +0200

    fix på tabeller i HentEventLogRes + opprettet statistikkView ++

--- Straisimulator/Controllers/HomeController.cs
+++ Straisimulator/Controllers/HomeController.cs
@@ -36,13 +36,19 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult HentEventLogError()
+    {
+        return View();
+    }
+    
 
     public IActionResult HentEventLogRes(String orderId)
     {
         ProductionEventList productionEvents = _dataFetchService.FetchProductionEvents(orderId);
         if (productionEvents.ProductionEvents.Count == 0)
         {
-            throw new InvalidOperationException("There are no events in the list");
+            return View("HentEventLogError");
         }
         else
         {
@@ -52,32 +58,27 @@ public class HomeController : Controller
             return View(model);
         }
     }
-    public IActionResult Resultater()
+
+    public IActionResult Statistikk()
     {
-        
-        List<SkapVegg> skap = new List<SkapVegg>();
-        
-        SkapVegg skap1 = new SkapVegg(8333, "Vetilatorskap for Slimline H:72,0 cm", 82, 65, 83, 67, 64, 32,65, 54);
-        SkapVegg skap2 = new SkapVegg(8332, "Overskap 80 cm m/2 dører H:72.0 cm", 85, 89, 86, 90,64, 32,65, 54);
-        SkapVegg skap3 = new SkapVegg(8331, "Overskap 80 cm m/2 dører H:72.0 cm", 85, 81, 86, 82,64, 32,65, 54);
-        SkapVegg skap4 = new SkapVegg(8330, "Overskap 80 cm m/2 dører H:72.0 cm", 80, 89, 81, 91,64, 32,65, 54);
-        SkapVegg skap5 = new SkapVegg(8329, "Overskap 70 cm m/2 dører Retning = Venstre", 63, 71, 65, 72,64, 32,65, 54);
-        SkapVegg skap6 = new SkapVegg(8328, "Kolonialseksjon 80 cm m/3 skuffer", 120, 82, 137, 137, 64, 32, 65, 54);
-        SkapVegg skap7 = new SkapVegg(8327, "Hjørnebenk 100x60 cm m/50 cm dør", 49, 53, 71, 54, 64, 32, 65, 54);
-        skap.Add(skap1);
-        skap.Add(skap2);
-        skap.Add(skap3);
-        skap.Add(skap4);
-        skap.Add(skap5);
-        skap.Add(skap6);
-        skap.Add(skap7);
-
-        ResultaterViewModel model = new ResultaterViewModel();
-        model.Skap = skap;
-
-        return View(model);
-        
+        return View();
     }
+    public IActionResult StatistikkRes(string orderId)
+    {
+        ProductionEventList productionEvents = _dataFetchService.FetchProductionEvents(orderId);
+        if (productionEvents.ProductionEvents.Count == 0)
+        {
+            return View("HentEventLogError");
+        }
+        else
+        {
+            StatistikkResViewModel model = new StatistikkResViewModel();
+            model.ProductionEventList = productionEvents;
+            model.ProductionEventList.OrderId = orderId;
+            return View(model);
+        }
+    }
+    
     
     public IActionResult Login()
     {
